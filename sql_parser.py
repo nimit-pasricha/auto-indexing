1a2c582ed497d9ace8fd3788e6bf2c02d8d74f2a
nimit-pasricha <pasricha.nimit@gmail.com>
2025-12-30 17:53:24 +0400

    Add parsing for IS and IN.

--- sql_parser.py
+++ sql_parser.py
@@ -26,12 +26,12 @@ def extract_metadata(sql: str):
     # 2. Extract WHERE clause details
     for token in parsed.tokens:
         if isinstance(token, Where):
-            for condition in token.tokens:
+            for i, condition in enumerate(token.tokens):
                 if isinstance(condition, Comparison):
                     # item.left is the column, item.value is the whole comparison
                     # e.g., "age > 25" -> left is "age", operator is ">"
                     col = condition.left.value
-                    # Extract the operator (>, <, =, etc.)
+                    # Extract the operator (>, <=, =, etc.)
                     operator = ""
                     for t in condition.tokens:
                         if t.ttype == sqlparse.tokens.Operator.Comparison:
@@ -42,9 +42,24 @@ def extract_metadata(sql: str):
                         "operator": operator.strip()
                     })
 
+                elif condition.value.upper() == "IS":
+                    prev_item = token.token_prev(i, skip_cm=True)
+                    next_item = token.token_next(i, skip_cm=True)
+                    metadata["filters"].append({
+                        "column": prev_item[1].value,
+                        "operator": "IS NULL" if "NOT" not in next_item[1].value else "IS NOT NULL"
+                    })
+                
+                elif condition.ttype is Keyword and condition.value.upper() == 'IN':
+                    prev_item = token.token_prev(i, skip_cm=True)
+                    metadata["filters"].append({
+                        "column": prev_item[1].value,
+                        "operator": "IN"
+                    })
+
     return metadata
 
 # --- TEST IT ---
-test_sql = "SELECT * FROM users WHERE age >= 25 AND city = 'New York'"
+test_sql = "SELECT * FROM users WHERE age IN ('A', 'B', 'C')"
 data = extract_metadata(test_sql)
 print(data)
