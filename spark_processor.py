7ce0dcd2a6f47d6074b439bdab3ca1c64e104282
Nimit Pasricha <pasricha.nimit@gmail.com>
2026-01-02 14:59:46 +0400

    Define kafka msg schema for spark streaming.

---
+++ spark_processor.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, window, count
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+
+spark = (
+    SparkSession.builder.appName("IndexOptimizer")
+    .master("spark://localhost:7077")
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
+    .get_or_create()
+)
+
+schema = StructType(
+    [
+        StructField("table", StringType()),
+        StructField("column", StringType()),
+        StructField("operator", StringType()),
+        StructField("timestamp", DoubleType()),
+    ]
+)
+
