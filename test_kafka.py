93a19ca516fc615b563c7b0b67d3bda952e6130f
Nimit Pasricha <pasricha.nimit@gmail.com>
2026-01-02 20:43:57 +0400

    Fix spark in container not reading from kafka.

--- test_kafka.py
+++ test_kafka.py
@@ -3,7 +3,7 @@ import time
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['localhost:9094'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
